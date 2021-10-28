3ef27ffee50f05cf262fac74f6c7f61e5d46b83a
FranckyPoop <afrifranck2003@gmail.com>
2021-10-28 23:30:40 +0000

    fin du menu

--- time.py
+++ time.py
@@ -2,19 +2,35 @@ import fonction
 
 # On récupere les profiles
 profile_recuperer = fonction.recuperation_profile()
+print(profile_recuperer)
 
 # On demande a l'utilisateur de choisir un pofile
-# On affiche tout les profile qui son enregistré
-profile = input("Veuillez entrer le nom de votre profile pour continuer: ")
+print("Veuillez choisir un profile")
 
-if profile not in profile_recuperer.keys():
-    reponse = input("Votre profile n'exixte pas voulez vous crée un profile O/N")
+# On affiche tout les profile qui son enregistré
+for cle in profile_recuperer.keys():
+    print(cle)
+    
+choix_profile = input("Veuillez entrer le nom de votre profile pour continuer: ")
+
+# On enregistre l'utilisateur si le profile n'existe pas
+if choix_profile not in profile_recuperer.keys():
+    reponse = input("Votre profile n'exixte pas voulez vous crée un profile O/N: ")
     
     # on enregistre le profile
-    if reponse == "O":
+    if reponse.upper() == "O":
         # on verfie si le profile de l'utilisateur est correct 
         validation_profile = fonction.validation_profile()
         
         # on enregistre le profile
         profile_recuperer[validation_profile] = None
         fonction.enregistrement_profile(profile_recuperer)
+        
+# On affiche le menu a l'utilisateur si le profile choisir ou crée est dans la liste des profile
+if choix_profile or validation_profile in profile_recuperer.keys():
+    # Menu creation du menu
+    # On affiche des option au menu
+    print("---------- Bienvenue dans le menu du chrono ----------") 
+    print("1- Démarer le chrone")
+    print("2- Voir l'historique")
+    print("3- Choisir un autres profile")
