d9da9d1138ffb16db0a18914617aee29e57cfb89
FranckyPoop <afrifranck2003@gmail.com>
2022-01-08 19:49:35 +0000

    Ajout de l'option 1 au programme

--- main.py
+++ main.py
@@ -9,54 +9,51 @@ folder_data_programmme = os.path.join(path_folder, "data_programme")
 
 if not os.path.exists(folder_data_programmme):
     data.data_recording([], path_folder, "data_programme", "profile_list")
-
 choice_profile = input("Veuillez entrer le nom de vôtre profile: ").lower()
-profile_exist = False
-       
-while profile_exist == False:
-    # Récupération de la liste des profiles
-    profile_list_recovery = data.get_data(folder_data_programmme, "profile_list")
+while True:
     
-    for item in profile_list_recovery:
-        if item["nom_profile"] == choice_profile:
-            profile_exist = True
-
-    if not profile_exist:
-        choice = input("Le profile n'existe pas ! Voulez vous crée un profile ? O/N").upper()
+    profile_exist = False
         
-        if choice == "O":
-            name_profile = fonction.validation_profile()
-            instance_profile = {"nom_profile": name_profile, "total_time": 0}
-            profile_list_recovery.append(instance_profile)
+    while profile_exist == False:
+        # Récupération de la liste des profiles
+        profile_list_recovery = data.get_data(folder_data_programmme, "profile_list")
+        
+        for profile in profile_list_recovery:
+            if profile["nom_profile"] == choice_profile:
+                profile_exist = True
+
+        if not profile_exist:
+            choice = input("Le profile n'existe pas ! Voulez vous crée un profile O/N ? ").upper()
+            
+            if choice == "O":
+                name_profile = fonction.validation_profile()
+                instance_profile = {"nom_profile": name_profile, "total_time": 0}
+                profile_list_recovery.append(instance_profile)
+                print(profile_list_recovery)
+                profile_exist = True
+                print("Vous venez de crée un nouveau profile")
+                data.data_recording(profile_list_recovery, path_folder, "data_programme", "profile_list")
+                time.sleep(1.5)
+        elif profile_exist:
+            menu = """
+            ---------- Bienvenue dans le menu ----------
+            1- Démarer le chronométre
+            2- Voir l'historique
+            3- Changer de profile
+            4- Quitter le programme
+            """
+            print(menu)
+            
+            choice_menu = int(input())
+            
+            if choice_menu == 1:
+                stopwatch = fonction.stopwatch()
+                profile["total_time"] += stopwatch
+                data.data_recording(profile_list_recovery, path_folder, "data_programme", "profile_list")
+            elif choice_menu == 2:
+                pass
             print(profile_list_recovery)
-            profile_exist = True
-            print("Vous venez de crée un nouveau profile")
-            data.data_recording(profile_list_recovery, path_folder, "data_programme", "profile_list")
-            time.sleep(1.5)
-        else:
-            break
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-
-# while profile_exist:
-#     if profile_exist:
-#         menu = """
-#         ---------- Bienvenue dans le menu ----------
-#         1- Démarer le chronométre
-#         2- Voir l'historique
-#         3- Changer de profile
-#         """
-#         print(menu)
-#         reponse = int(input())
+                
+            
+             
 
