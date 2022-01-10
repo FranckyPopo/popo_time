a5a99e0631093dba2565159a3a8c44bb4ac69749
FranckyPoop <afrifranck2003@gmail.com>
2022-01-10 19:21:48 +0000

    Fin de la créatiion de la fonctionnalité voir l'historique

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import fonction
 import os
 import data
 import time
+from pprint import pprint
 
 menu = """
 ---------- Bienvenue dans le menu ----------
@@ -9,6 +10,11 @@ menu = """
 2- Voir l'historique
 3- Changer de profile
 4- Quitter le programme
+"""
+display_history = """
+Date: {}
+Temps: {}
+----------------------
 """
 
 path_file = os.path.realpath(__file__)
@@ -22,29 +28,25 @@ if not os.path.isfile(file_history) and not os.path.isfile(file_profile_list):
     data.data_recording([], path_folder, "data_programme", "history")
 
 profile_exist = False
-
-# Récupération des différentes données
 profile_list_recovery = data.get_data(folder_data_programmme, "profile_list")
-history = data.get_data(folder_data_programmme, "history")
 
 # On vérifie que le profile existe dans la liste des profiles
 choice_profile = input("Veuillez entrer le nom de vôtre profile: ").lower()
 for item in profile_list_recovery:
     if item["name_profile"] == choice_profile:
-        choice_profile = item
         profile_exist = True
         break
         
 while True:
-                    
+
     while not profile_exist:
         choice = input("Le profile n'existe pas ! Voulez vous crée un profile O/N ? ").upper()
 
         if choice == "O":
             name_profile = fonction.validation_profile()
+            #choice_profile = name_profile
             instance_profile = {"name_profile": name_profile, "total_time": 0}
             profile_list_recovery.append(instance_profile)
-            print(profile_list_recovery)
             profile_exist = True
             print("Vous venez de crée un nouveau profile")
             data.data_recording(profile_list_recovery, path_folder, "data_programme", "profile_list")
@@ -55,6 +57,7 @@ while True:
     while profile_exist:
         print(menu)  
         choice_menu = int(input())
+        history = data.get_data(folder_data_programmme, "history") 
         
         if choice_menu == 1:
             stopwatch = fonction.stopwatch()
@@ -62,5 +65,14 @@ while True:
             fonction.history(item["name_profile"], stopwatch)
             data.data_recording(profile_list_recovery, path_folder, "data_programme", "profile_list")    
         elif choice_menu == 2:
-            pass
-
+            history_profile = [item for item in history if item["name_profile"] == choice_profile]
+            
+            for item in history_profile:
+                hour = item["time"] // 3600
+                rest_hour = item["time"] % 3600
+                minute = rest_hour // 60
+                seconde = rest_hour % 60 
+                time_total = f"{hour}: {minute}: {seconde}"
+                print(display_history.format(item["date"], time_total))
+                time.sleep(0.5)
+                
