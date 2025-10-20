5c80273c29edb7bf9495c7872be8d407e634da02
Afri Franck <afrifranck2003@gmail.com>
2025-10-20 15:33:45 +0000

    COMMIT BAD

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ from popo_time import ProgramMenu
 if __name__ == "__main__":
     main = ProgramMenu()
     main.program_start()
-    print('FRANCKY POPO IS THE BEST')
+    print('FRANCKY POPO IS THE BAD')
